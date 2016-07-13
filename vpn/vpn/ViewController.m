ac985293dd71302d97ad53514e59da7a776bcbb3
游庆盛 <youqingsheng@wsncm.com>
2016-07-13 15:25:35 +0800

    aa

---
+++ vpn/vpn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  vpn
+//
+//  Created by Yqs on 16/7/13.
+//  Copyright © 2016年 wsn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
